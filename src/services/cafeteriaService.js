26cc600393914910448026e5d51ee5a6fd15020e
arcuar <nunnaerim@gmail.com>
2026-03-24 21:50:39 +0900

    init

---
+++ src/services/cafeteriaService.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+// shop_sqno={숫자} 이걸로 식당 구분
+const URL = "https://coop.knu.ac.kr/sub03/sub01_01.html?shop_sqno=35";
+
+const cafeteriaService = () => {};
